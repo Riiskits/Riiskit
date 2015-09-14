65261e56a132ad6e1b0a0c8f7ac5fb0ea12c70f9
chrisriis <christoffer.riis@gmail.com>
2015-09-14 04:10:46 +0200

    Add livereload with BrowserSync and image compression.

--- gulpfile.js
+++ gulpfile.js
@@ -1,75 +1,115 @@
 
 // Author: halvard@mekom.no
 
-var gulp       = require('gulp');
-var sass       = require('gulp-sass');
-var concat     = require('gulp-concat');
-var cssmin     = require('gulp-minify-css');
-var rename     = require("gulp-rename");
-var uglify     = require('gulp-uglify');
-var changed    = require('gulp-changed');
-var livereload = require('gulp-livereload');
-var plumber    = require('gulp-plumber');
-var scsslint   = require('gulp-scss-lint');
-var sourcemaps = require('gulp-sourcemaps');
-var jshint     = require('gulp-jshint');
-var stylish    = require('jshint-stylish');
-var notify     = require('gulp-notify');
+var gulp        = require('gulp');
+var sass        = require('gulp-sass');
+var concat      = require('gulp-concat');
+var cssmin      = require('gulp-minify-css');
+var rename      = require("gulp-rename");
+var uglify      = require('gulp-uglify');
+var changed     = require('gulp-changed');
+var browserSync = require('browser-sync').create();
+var plumber     = require('gulp-plumber');
+var scsslint    = require('gulp-scss-lint');
+var jshint      = require('gulp-jshint');
+var stylish     = require('jshint-stylish');
+var imagemin    = require('gulp-imagemin');
+var pngquant    = require('imagemin-pngquant');
+var notify      = require('gulp-notify');
 
 var paths = {
     //source paths
-    scss: 'src/scss/**/*.scss',
-    js: 'src/js/plugins.js, src/js/**/*.js',
+    scss: [
+        'src/scss/base/normalize.scss',
+        'src/scss/utilities/helper-classes.scss',
+        'src/scss/**/*.scss'
+    ],
+    js: 'src/js/**/*.js',
+    img: 'src/img/*',
     //distribution paths
     cssDst: 'dist/css/',
-    jsDst: 'dist/js/'
+    jsDst: 'dist/js/',
+    imgDst: 'dist/img/'
 };
 
-gulp.task('styles', function () {
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        host: 'localhost/',
+        proxy: '127.0.0.1:8888/',
+        port: 8888,
+        open: true,
+    });
+});
+
+gulp.task('styles', function() {
     return gulp.src(paths.scss)
-        .pipe(sass({includePaths: require('node-bourbon').includePaths}))
-        .pipe(plumber())
+        .pipe(sass({
+            includePaths: require('node-bourbon').includePaths
+        }))
+        .pipe(plumber({
+            errorHandler: notify.onError("Error: <%= error.message %>")
+        }))
         .pipe(gulp.dest(paths.cssDst))
         .pipe(cssmin())
         .pipe(concat('main.css'))
         .pipe(rename({
             suffix: '.min'
         }))
-        .pipe(gulp.dest(paths.cssDst + 'min/'));
+        .pipe(gulp.dest(paths.cssDst + 'min/'))
+        .pipe(notify('Generated CSS file: <%= file.relative %>'))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('lint', function(){
-    //sass lint
-    gulp.src(paths.scss)
-    .pipe(scsslint());
-    //jshint
+gulp.task('js', function() {
+    return gulp.src(paths.js)
+        .pipe(plumber({
+            errorHandler: notify.onError("Error: <%= error.message %>")
+        }))
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest(paths.jsDst))
+        .pipe(uglify())
+        .pipe(rename({
+          suffix: '.min'
+        }))
+        .pipe(gulp.dest(paths.jsDst + 'min/'))
+        .pipe(notify('Generated JS file: <%= file.relative %>'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('images', function() {
+    return gulp.src(paths.img)
+        .pipe(plumber({
+            errorHandler: notify.onError("Error: <%= error.message %>")
+        }))
+        .pipe(imagemin({
+            progressive: true,
+            interlaced: true,
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest(paths.imgDst))
+        .pipe(notify('Images compressed.'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('jslint', function() {
     gulp.src(paths.js)
-    .pipe(jshint())
-    .pipe(jshint.reporter(stylish));
+        .pipe(jshint())
+        .pipe(jshint.reporter(stylish))
+        .pipe(jshint.reporter('fail'))
+        .on('error', notify.onError({ message: 'JS hint fail'}));
 });
 
-gulp.task('scripts', function () {
-   return gulp.src(paths.js)
-    .pipe(plumber())
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest(paths.jsDst))
-    .pipe(uglify())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest(paths.jsDst + 'min/'));
+gulp.task('scsslint', function() {
+    gulp.src(paths.scss)
+        .pipe(scsslint());
 });
 
-gulp.task('watch', function(){
-    gulp.watch(paths.js, function(){
-        gulp.start('scripts');
-    });
-    gulp.watch(paths.scss, function(){
-        gulp.start('styles');
-    });
+gulp.task('watch', function() {
+    gulp.watch('./').on('change', browserSync.reload);
+
+    gulp.watch(paths.js, ['js', 'jslint', 'browser-sync']);
+    gulp.watch(paths.scss, ['styles', 'browser-sync']);
+    gulp.watch(paths.img, ['images', 'browser-sync']);
 });
 
-gulp.task('default', function() {
-    gulp.start('scripts');
-    gulp.start('styles');
-});
+gulp.task('default', ['watch', 'browser-sync']);
