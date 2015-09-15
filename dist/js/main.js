224faafed6c377abe32f8572b744e2e44e507d51
chrisriis <christoffer.riis@gmail.com>
2015-09-15 15:58:47 +0200

    Add debounce/throttle helpers.

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,6 +7,47 @@
 
 'use strict';
 
+(function (b, c) {
+  var $ = b.jQuery || b.Cowboy || (b.Cowboy = {}),
+      a;$.throttle = a = function (e, f, j, i) {
+    var h,
+        d = 0;if (typeof f !== "boolean") {
+      i = j;j = f;f = c;
+    }function g() {
+      var o = this,
+          m = +new Date() - d,
+          n = arguments;function l() {
+        d = +new Date();j.apply(o, n);
+      }function k() {
+        h = c;
+      }if (i && !h) {
+        l();
+      }h && clearTimeout(h);if (i === c && m > e) {
+        l();
+      } else {
+        if (f !== true) {
+          h = setTimeout(i ? k : l, i === c ? e - m : e);
+        }
+      }
+    }if ($.guid) {
+      g.guid = j.guid = j.guid || $.guid++;
+    }return g;
+  };$.debounce = function (d, e, f) {
+    return f === c ? a(d, e, false) : a(d, f, e !== false);
+  };
+})(undefined);
+
+/**
+ * Better way to cache jQuery selectors.
+ *
+ * Usage: $.selector_cache('.element');
+ *
+ * @link https://eamann.com/tech/selector-caching-jquery/#comment-41220
+ * @todo https://gist.github.com/jtsternberg/14978579a9edf42ed069
+ *
+ * @since Riiskit 1.0.0
+ */
+
 (function ($) {
   $.selector_cache = function (selector) {
     if (!$.selector_cache[selector]) {
@@ -44,13 +85,13 @@
     }, false);
   }
 })();
-/**
- * Better way to cache jQuery selectors.
+/*
+ * jQuery throttle / debounce - v1.1 - 3/7/2010
+ * http://benalman.com/projects/jquery-throttle-debounce-plugin/
  *
- * Usage: $.selector_cache('.element');
- *
- * @link https://eamann.com/tech/selector-caching-jquery/#comment-41220
- * @todo https://gist.github.com/jtsternberg/14978579a9edf42ed069
+ * Copyright (c) 2010 "Cowboy" Ben Alman
+ * Dual licensed under the MIT and GPL licenses.
+ * http://benalman.com/about/license/
  *
  * @since Riiskit 1.0.0
  */
