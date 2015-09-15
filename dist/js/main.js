57aa87e62308895cc619ea628773b33ce0ebb07b
chrisriis <christoffer.riis@gmail.com>
2015-09-15 15:55:42 +0200

    Remove console.log helper in browsers that don't support it.

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,56 +5,17 @@
  * @since Riiskit 1.0.0
  */
 
-/**
- * Avoid console errors in browsers that lack a console.
- *
- * @since Riiskit 1.0.0
- */
+'use strict';
 
-( function(){
-    var method;
-    var noop = function () {};
-    var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
-    ];
-    var length = methods.length;
-    var console = (window.console = window.console || {});
-
-    while (length--) {
-        method = methods[length];
-
-        // Only stub undefined methods.
-        if (!console[method]) {
-            console[method] = noop;
-        }
+(function ($) {
+  $.selector_cache = function (selector) {
+    if (!$.selector_cache[selector]) {
+      $.selector_cache[selector] = $(selector);
     }
-}());
 
-
-/**
- * Better way to cache jQuery selectors.
- *
- * Usage: $.selector_cache('.element');
- *
- * @link https://eamann.com/tech/selector-caching-jquery/#comment-41220
- * @todo https://gist.github.com/jtsternberg/14978579a9edf42ed069
- *
- * @since Riiskit 1.0.0
- */
-
-( function ($){
-	$.selector_cache = function (selector) {
-		if ( ! $.selector_cache[selector] ) {
-			$.selector_cache[selector] = $(selector);
-		}
-
-		return $.selector_cache[selector];
-	};
-}(jQuery));
-
+    return $.selector_cache[selector];
+  };
+})(jQuery);
 
 /**
  * Makes "skip to content" link work correctly in IE9, Chrome, and Opera
@@ -65,22 +26,31 @@
  * @since Riiskit 1.0.0
  */
 
-( function (){
-	var ua = navigator.userAgent.toLowerCase();
+(function () {
+  var ua = navigator.userAgent.toLowerCase();
 
-	if ( ( ua.indexOf( 'webkit' ) > -1 || ua.indexOf( 'opera' ) > -1 || ua.indexOf( 'msie' ) > -1 ) &&
-		document.getElementById && window.addEventListener ) {
+  if ((ua.indexOf('webkit') > -1 || ua.indexOf('opera') > -1 || ua.indexOf('msie') > -1) && document.getElementById && window.addEventListener) {
 
-		window.addEventListener( 'hashchange', function() {
-			var element = document.getElementById( location.hash.substring( 1 ) );
+    window.addEventListener('hashchange', function () {
+      var element = document.getElementById(location.hash.substring(1));
 
-			if ( element ) {
-				if ( ! /^(?:a|select|input|button|textarea)$/i.test( element.nodeName ) ) {
-					element.tabIndex = -1;
-				}
+      if (element) {
+        if (!/^(?:a|select|input|button|textarea)$/i.test(element.nodeName)) {
+          element.tabIndex = -1;
+        }
 
-				element.focus();
-			}
-		}, false );
-	}
-}());
+        element.focus();
+      }
+    }, false);
+  }
+})();
+/**
+ * Better way to cache jQuery selectors.
+ *
+ * Usage: $.selector_cache('.element');
+ *
+ * @link https://eamann.com/tech/selector-caching-jquery/#comment-41220
+ * @todo https://gist.github.com/jtsternberg/14978579a9edf42ed069
+ *
+ * @since Riiskit 1.0.0
+ */
--- src/js/plugins.js
+++ src/js/plugins.js
@@ -6,35 +6,6 @@
  */
 
 /**
- * Avoid console errors in browsers that lack a console.
- *
- * @since Riiskit 1.0.0
- */
-
-( function(){
-    var method;
-    var noop = function () {};
-    var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
-    ];
-    var length = methods.length;
-    var console = (window.console = window.console || {});
-
-    while (length--) {
-        method = methods[length];
-
-        // Only stub undefined methods.
-        if (!console[method]) {
-            console[method] = noop;
-        }
-    }
-}());
-
-
-/**
  * Better way to cache jQuery selectors.
  *
  * Usage: $.selector_cache('.element');
